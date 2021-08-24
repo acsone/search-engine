6653aaf8aff1ad878cbf041e439d68cad867b763
Cédric Pigeon <cedric.pigeon@acsone.eu>
2021-08-24 07:15:32 +0200

    [FIX] add missing init script for tech_name on algolia backend

--- connector_algolia/migrations/13.0.2.2.0/post-migrate.py
+++ connector_algolia/migrations/13.0.2.2.0/post-migrate.py
@@ -1,9 +1,22 @@
 # Copyright 2020 Camptocamp SA (http://www.camptocamp.com).
 # @author Simone Orsi <simahawk@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
 
+from odoo import SUPERUSER_ID, api
 from odoo.tools.sql import set_not_null
+
+_logger = logging.getLogger(__name__)
 
 
 def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    for backend in env["se.backend.algolia"].search([("tech_name", "=", False)]):
+        backend._onchange_name_for_tech()
+        _logger.info(
+            "Backend '%s' `tech_name` set automatically. "
+            "Please check if it suits your env.",
+            backend.name,
+        )
+        backend.flush()
     set_not_null(cr, "se_backend_algolia", "tech_name")
